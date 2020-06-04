64fbc487852dac2789059627134001ee26bdca61
GopiR <getgopi@gmail.com>
2020-06-04 17:47:34 -0400

    Completed implementing and testing Harmonic.java

--- project1/src/main/edu/umb/cs210/p1/Harmonic.java
+++ project1/src/main/edu/umb/cs210/p1/Harmonic.java
@@ -6,14 +6,16 @@ public class Harmonic {
     // Computes the nth harmonic number, where n is passed in through args
     protected static Rational harmonicSum(String[] args) {
         // Get n from command line as integer.
-
+        int n = Integer.parseInt(args[0]);
 
         // Set total to the rational number 0.
         Rational total = new Rational(0);
 
         // For each 1 <= i <= n, add the rational term
         // 1 / i to total.
-
+        for (int i = 1; i <= n ; i++) {
+            total = total.add(new Rational(1, i));
+        }
 
          return total;
     }
