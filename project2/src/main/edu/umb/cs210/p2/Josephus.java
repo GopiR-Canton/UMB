e26a1129fa4f135e2d0f27d38750121d02614245
GopiR <getgopi@gmail.com>
2020-06-12 12:23:33 -0400

    Implemented Josephus.java

--- project2/src/main/edu/umb/cs210/p2/Josephus.java
+++ project2/src/main/edu/umb/cs210/p2/Josephus.java
@@ -7,19 +7,37 @@ import edu.princeton.cs.algs4.StdOut;
 public class Josephus {
     protected static LinkedQueue<Integer> calculateJosephus(String[] args) {
         // Get N and M from command line as ints.
+        int N = Integer.parseInt(args[0]);
+        int M = Integer.parseInt(args[1]);
 
         // Create a queue q and enqueue integers
         // 1, 2, ... N.
+        LinkedQueue<Integer> q = new LinkedQueue<>();
+        for (int i = 1; i <= N; i++) {
+            q.enqueue(i);
+        }
 
         // Create a queue outQ to store the ordering
-
+        LinkedQueue<Integer> outQ = new LinkedQueue<>();
 
         // As long as q is not empty: increment i;
         // dequeue an element pos; if M divides i,
         // enqueue pos to outQ, otherwise enqueue pos to q.
+        int i = 1;
+        while (!q.isEmpty()){
+            Integer pos = q.dequeue();
+            if (i % M == 0){
+                outQ.enqueue(pos);
+            }
+            else{
+                q.enqueue(pos);
+            }
+
+            i++;
+        }
 
         // Return outQ
-        return new LinkedQueue<>();
+        return outQ;
     }
 
     // Entry point. [DO NOT EDIT]
